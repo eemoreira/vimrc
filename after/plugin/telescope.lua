523c862204af0a22bff4c267999a003a19010825
emoreira <theeduardogi682@gmail.com>
2025-10-26 15:02:51 -0300

    telescope: add some usefull keybinds

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,8 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+local map = vim.keymap.set
+map('n', "<leader>f",  builtin.find_files,           { desc = 'Telescope find files' })
+map('n', "<leader>g",  builtin.live_grep,            { desc = 'Telescope live grep' })
+map("n", "<leader>si", builtin.grep_string,          { desc = 'Telescope grep string'})
+map("n", "<leader>sr", builtin.lsp_references,       { desc = 'Telescope LSP references' })
+map("n", "<leader>sd", builtin.lsp_definitions,      { desc = 'Telescope LSP definitions' })
+map("n", "<leader>ss", builtin.lsp_document_symbols, { desc = 'Telescope LSP document symbols' })
