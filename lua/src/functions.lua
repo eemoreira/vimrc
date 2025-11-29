35ccf16a111ac96ab89187d56d905624aee28bd8
emoreira <theeduardogi682@gmail.com>
2025-11-29 15:04:40 -0300

    feat: floating window

--- lua/src/functions.lua
+++ lua/src/functions.lua
@@ -1,22 +1,24 @@
 -- go back to last position on buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    local last_pos = vim.fn.line([['"]])
-    if last_pos > 0 and last_pos <= vim.fn.line("$") then
-      vim.cmd('normal! g`"')
-      vim.cmd('normal! zz')
-    end
-  end,
+    pattern = "*",
+    callback = function()
+        local last_pos = vim.fn.line([['"]])
+        if last_pos > 0 and last_pos <= vim.fn.line("$") then
+            vim.cmd('normal! g`"')
+            vim.cmd('normal! zz')
+        end
+    end,
 })
 
 -- auto-complete
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method("textDocument/completion") then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
-	end,
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client then
+            if client:supports_method("textDocument/completion") then
+                vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+            end
+        end
+    end,
 })
 vim.cmd("set completeopt+=noselect")
